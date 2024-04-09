eb90f71120f16d0a0781af759b0fc8d902d3071d
mmorales68 <miguelm4032@gmail.com>
2024-04-08 21:35:08 -0600

    Inicializacion del directorio con los archivos principales

---
+++ Explorador de archivos/funciones.h
@@ -0,0 +1,17 @@
+//
+// Created by mmora on 8/04/2024.
+//
+
+#ifndef EXPLORADOR_DE_ARCHIVOS_FUNCIONES_H
+#define EXPLORADOR_DE_ARCHIVOS_FUNCIONES_H
+
+#endif //EXPLORADOR_DE_ARCHIVOS_FUNCIONES_H
+
+#include "iostream"
+#include "filesystem"
+using namespace std;
+
+void hola(){
+    cout << "hola" << endl;
+
+}
